3d4f77b38b2f1edd5769b4ac99d3fc0f420457c0
Felix Schultz <felix.schultz@good-co.de>
2026-03-25 22:06:42 +0100

    fix: update macOS platform version to 10.15 in Package.swift

--- ios/Package.swift
+++ ios/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "tauri-plugin-remote-push",
   platforms: [
-    .macOS(.v10_13),
+    .macOS(.v10_15),
     .iOS(.v13),
   ],
   products: [
